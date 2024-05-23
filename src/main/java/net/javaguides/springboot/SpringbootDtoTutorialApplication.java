e586b138e0233cd885cd7dc5360a47668e4bf15a
Susanta <susant@gmail.com>
2024-05-22 21:41:03 -0400

    first commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootDtoTutorialApplication.java
@@ -0,0 +1,55 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.model.Location;
+import net.javaguides.springboot.model.User;
+import net.javaguides.springboot.repository.LocationRepository;
+import net.javaguides.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDtoTutorialApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDtoTutorialApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private LocationRepository locationRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Location location = new Location();
+		location.setPlace("pune");
+		location.setDescription("pune is a great place to livee");
+		location.setLongitude(23.4);
+		location.setLatitude(56.5);
+		locationRepository.save(location);
+
+		User user1 = new User();
+		user1.setFirstName("Susant");
+		user1.setLastName("Sigdel");
+		user1.setEmail("susantsigdel@gmail.com");
+		user1.setPassword("secret");
+		user1.setLocation(location);
+		userRepository.save(user1);
+
+		User user2 = new User();
+		user2.setFirstName("Salim");
+		user2.setLastName("Mah");
+		user2.setEmail("saliml@gmail.com");
+		user2.setPassword("secret1");
+		user2.setLocation(location);
+		userRepository.save(user2);
+
+
+
+
+	}
+
+}
